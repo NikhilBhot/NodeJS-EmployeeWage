4cab7ad0a9154d3b703cf97624fa039678710755
unknown <bhotnikhil25@gmail.com>
2023-01-24 00:55:46 +0530

    [Nikhil]-Regactor Employee Wage Using Function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,33 +4,36 @@
 // - Assume Part Time is 4 Hrs and Full time is 8 Hrs and per hour wage is $20
 // - Solve Using Switch Statement
 
+//UC-03-Refactored code to write function for daily working Hours
 
-
-const IS_ABSENT = 0
-const IS_PART_TIME = 1;
-const IS_FULL_TIME = 2;
+// const IS_ABSENT = 0
+ const IS_PART_TIME = 1;
+ const IS_FULL_TIME = 2;
 const IS_PART_TIME_HOURS = 4;
 const IS_FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_ABSENT) {
-    console.log("Employee Is Absent");
-    return;
-}
-else {
-    console.log("Emloyee Is Present");
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    switch (empCheck) {
-        case IS_PART_TIME:
-            console.log("Part Time Employee");
-            empHrs = IS_PART_TIME_HOURS;
-            break;
-        case IS_FULL_TIME:
-            console.log("Full Time Employee");
-            empHrs = IS_FULL_TIME_HOURS
-            break;
+
+
+function getWorkingHour(empCheck) 
+{
+    
         
-    }
-    let empWage=empHrs*WAGE_PER_HOUR;
-    console.log("Employee Wage:"+empWage);
+        switch (empCheck) {
+            case IS_PART_TIME:
+                //console.log("Part Time Employee");
+                return IS_PART_TIME_HOURS;
+                break;
+            case IS_FULL_TIME:
+                //console.log("Full Time Employee");
+                return IS_FULL_TIME_HOURS
+                break;
+            default:
+                return 0;
+        }
+    
 }
+let empHrs=0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+empHrs = getWorkingHour(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage:" + empWage);
